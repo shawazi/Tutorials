349b42edfd662c3e23c94de68ebc1b9a5758bf6d
[shawazimam] <[shawazisonfire@gmail.com]>
2022-06-17 22:12:02 -0400

    progress with OpenAI SaaS tutorial, unrelated pwd generator added

---
+++ OpenAI/attempt_cdk/lambda_base_layer/generate_base_layer.sh
@@ -0,0 +1,18 @@
+ 
+# generates a base layer for the lambda functions
+
+# remove the container first (if it exists)
+  
+ docker rm layer-container
+
+#build the base layer 
+
+ docker build -t base-layer .
+
+#rename it to layer-container
+
+ docker run --name layer-container base-layer
+
+# copy the generated zip artifact so our cdk can use it
+
+ docker cp layer-container:layer.zip . && echo "Created layer.zip with updated base layer."
