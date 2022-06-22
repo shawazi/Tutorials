a2318594b8736c78e227fbd54ca4325a93bbb32b
[shawazimam] <[shawazisonfire@gmail.com]>
2022-06-22 11:48:02 -0400

    functional

--- Intermediate/expensetracker.py
+++ Intermediate/expensetracker.py
@@ -1,37 +1,103 @@
 from tkinter import *
 from tkinter import ttk
 import sqlite3 as db
-
 from tkcalendar import DateEntry
 
 def init():
-    connectionObjn = db.connect("expenses.db")
+    connectionObjn = db.connect("expense_tracker.db")
     curr = connectionObjn.cursor()
-    query = '''
-    Create table if expenses doesn't exist 
-        (date string, name string, title string, expense number)
+    query = ''' 
+    CREATE TABLE IF NOT EXISTS expense_tracker (
+        date string, 
+        title string, 
+        expense number
+        )
     '''
-
-curr.execute(query)
-connectionObjn.commit()
+    curr.execute(query)
+    connectionObjn.commit()
 
 def submitexpense():
     values = [dateEntry.get(), Name.get(), Title.get(), Expense.get()]
     print(values)
     Etable.insert('', 'end', values=values)
 
-    connectionObjn = db.connect(expenses.db)
+    connectionObjn = db.connect("expense_tracker.db")
     curr = connectionObjn.cursor()
     query = '''
-    Insert values into expenses
+    Insert into expense values (?, ?, ?, ?)
     '''
-    curr.execute(query, (dateEntry.get(), Name.get(), Title.get(), Expense.get()))
+    curr.execute(query, values)
     connectionObjn.commit()
 
 def viewexpense():
-    connectionObjn = db.connect("expenses.db")
+    connectionObjn = db.connect("expense_tracker.db")
     curr = connectionObjn.cursor()
     query = '''
-    select total from expenses
+    Select * from expenses
     '''
+    total = ''' select sum(expense) from expenses'''
+    curr.execute(query)
+    rows = curr.fetchall()
+    curr.execute(total)
+    amount = curr.fetchall()[0]
+    print(rows)
+    print(amount)
+
+    l = Label(root, text = "Date\t Name\t Title\t Expense", font = 
+("Arial", 16, "bold"), bg = "DodgerBlue", fg = "white")
+    l.grid(row = 6, column = 0, padx = 7, pady = 7)
     
+    st = ""
+    for i in rows:
+        for j in i:
+            st += str(j) + "\t"
+        st += "\n"
+    print(st)
+    l = Label(root, text = st, font = ('Arial', 14))
+    l.grid(row = 7, column = 0, padx = 7, pady = 7)
+
+init()
+root = Tk()
+root.title("Expense Tracker")
+root.geometry('1000x800')
+
+dateLabel = Label(root, text = "Date", font = ("Arial", 16, "bold"), bg = "DodgerBlue", fg = "white", width = 12)
+dateLabel.grid(row = 0, column = 0, padx = 7, pady = 7)
+
+dateEntry = DateEntry(root, width = 12, front = ('Arial', 16, "bold"))
+dateEntry.grid(row = 0, column = 1, padx = 7, pady = 7)
+
+Name=StringVar()
+nameLabel = Label(root, text = "Name", font = ("Arial", 16, "bold"), bg = "DodgerBlue", fg = "white", width = 12)
+nameLabel.grid(row = 1, column = 0, padx = 7, pady = 7)
+
+NameEntry = Entry(root, textvariable=Name, font = ("Arial", 16, "bold"))
+NameEntry.grid(row = 1, column = 1, padx = 7, pady = 7)
+
+Title=StringVar()
+titleLabel=Label(root, text = "Title", font = ("Arial", 16, "bold"), bg = "DodgerBlue", fg = "white", width = 12)
+titleLabel.grid(row = 2, column = 0, padx = 7, pady = 7)
+
+titleEntry=Entry(root, textvariable=Title, font = ("Arial", 16, "bold"))
+titleEntry.grid(row = 2, column = 1, padx = 7, pady = 7)
+
+Expense=IntVar()
+expenseLabel=Label(root, text = "Expense", font = ("Arial", 16, "bold"), bg = "DodgerBlue", fg = "white", width = 12)
+expenseLabel.grid(row = 3, column = 0, padx = 7, pady = 7)
+
+expenseEntry = Entry(root, textvariable=Expense, font = ("Arial", 16, "bold"))
+expenseEntry.grid(row = 3, column = 1, padx = 7, pady = 7)
+
+submitbtn=Button(root, command = submitexpense, text = "Submit", font = ("Arial", 16, "bold"), bg = "DodgerBlue", fg = "white", width = 12)
+submitbtn.grid(row = 4, column = 0, padx = 13, pady = 13)
+viewtn=Button(root, command=viewexpense, text = "View expenses", font = ("Arial", 16, "bold"), bg = "DodgerBlue", fg = "white", width = 12)
+viewtn.grid(row = 4, column = 1, padx = 13, pady = 13)
+
+#all saved expenses
+Elist = ['Date', 'Name', 'Title', 'Expense']
+Etable = ttk.Treeview(root, columns = Elist, show = 'headings', height = 7)
+for c in Elist:
+    Etable.heading(c, text = c.title())
+Etable.grid(row = 5, column = 0, padx = 7, pady = 7, columnspan = 3)
+
+mainloop()
