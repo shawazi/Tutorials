1521de41befddf934f41089afddd419a95cda6f9
shawaz <shawazisonfire@gmail.com>
2022-07-03 21:03:02 -0400

    changed name of directory, using Grinberg Flask Mega-Tutorial

---
+++ microblog/app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+# app.secret_key = 'my-secret-key'  # temporary, while i figure out why the config file isn't working as intended
+
+from app import routes, models
---
+++ microblog/app/config.py
@@ -0,0 +1,7 @@
+"""Flask configuration."""
+from os import environ
+
+TESTING = True
+DEBUG = True
+FLASK_ENV = 'development'
+SECRET_KEY = 'test'
